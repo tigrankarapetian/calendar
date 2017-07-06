414330f14aab7daa0e3188ad6ac9d3f7ffdef12c
tigrankarapetian <337030gd@gmail.com>
2017-07-06 15:45:46 +0400

    Fixed the issue, when the rows and the days woudln't show up.
    - /HTML/ The places of 'Previous,' 'Next'  and the dropdown menus are switched.
    - /JS/ In Calendar.prototype.generateHTML generateRows() function has been altered, now it only generates the rows without filling them, the newly added fillTheRows() fills the rows instead.
    -/JS/ Added Weekday names in the calendar.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 window.onload = function () {
 	initMonths();
 	createYears();
-	Calendar(4, 2012);
-	Calendar.prototype.generateHTML();
+	initPrevButton();
+	var cal = new Calendar(6, 2017);
+	cal.generateHTML();
 }
 
-let currentDate = new Date();
 
+//globals
+
+let currentDate = new Date();
 let WEEK_DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 let MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 let MONTHS_DURATIONS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
 let START_YEAR = 1990;
 let END_YEAR = 2020;
 
-//done during class
-
 let thead = document.getElementsByTagName('thead');
+
+// Header
 
 function initMonths() {
 	let select = document.getElementsByTagName('select')[0];
@@ -51,45 +53,93 @@ function initPrevButton() {
 }
 
 
-//done by following steps in the guide @ http://jszen.blogspot.am/2007/03/how-to-build-simple-calendar-with.html
+
+// body
 
 function Calendar(month, year) {
 	this.month = (isNaN(month) || month == null) ? currentDate.getMonth() : month;
 	this.year = (isNaN(year) || year == null) ? currentDate.getFullYear() : year;
-// 	this.html = '';
-}
-
-Calendar.prototype.generateHTML = function() {
-	let firstDay = new Date(this.year, this.month, 1);
-	let startingDay = firstDay.getDay();
-	let monthLength = MONTHS_DURATIONS[this.month];
 	
-	if (this.month == 1) {
-		if (this.year % 4 === 0 && this.year % 100 != 0 || this.year % 400 === 0) {
-			monthLength = 29;
+	this.generateHTML = function() {
+		let firstDay = new Date(year, month, 0);
+		let startingDay = firstDay.getDay();
+		let monthLength = MONTHS_DURATIONS[month];
+		
+		if (this.month == 1) {
+			if (this.year % 4 === 0 && this.year % 100 != 0 || this.year % 400 === 0) {
+				monthLength = 29;
+			}
 		}
-	}
-	
-	function generateRows() {
-		let day = 1;
-		for (let i = 0; i < 9; i++) {
-			for (let j = 0; j < 6; j++) {
-				let tbody = document.getElementsByTagName('tbody');
+		
+		function getWeekdayNames() {
+			let createRow = document.createElement('tr');
+			createRow.setAttribute('id', 'weekdayNames');
+			let thead = document.getElementsByTagName('thead')[0];
+			thead.appendChild(createRow);
+			let weekdayRow = document.getElementById('weekdayNames');
+			for (let i = 0; i <= 6; i++) {
 				let createCell = document.createElement('td');
-				if (day <= monthLength && (i > 0 && j >= startingDay)) {
-					createCell.innerHTML = day;
-					day++;
-					tbody.appendChild(createCell);
-				}
-				
-				if(day > monthLength) {
-					break;
+				createCell.innerHTML = WEEK_DAYS[i];
+				weekdayRow.appendChild(createCell);
+			}
+		}
+		
+		getWeekdayNames();
+		
+		function generateRows() {
+			
+			let tbody = document.getElementById('tableBody');
+			//rows or weeks
+			let condition = (startingDay >= 4) ? 6 : 5;
+			for (let i = 0; i < condition; i++) {
+				let createRow = document.createElement('tr');
+				createRow.setAttribute('id', 'row' + i);
+				tbody.appendChild(createRow);
+				//columns or weekdays
+				for (let j = 0; j <= 6; j++) { 
+					let createCell = document.createElement('td');
+					let counter = i + 1;
+					createCell.setAttribute('id', 'cell' + i + j);
+					let currentRow = document.getElementById('row' + i);						
+					currentRow.appendChild(createCell);
 				}
 			}
 		}
+		
+		generateRows();
+		
+		function fillTheRows() {
+			let day = 1;
+			//rows
+			let condition = (startingDay >= 4) ? 6 : 5;
+			for (let i = 0; i < condition; i++) {
+				//columns or weekdays
+				let j = i === 0 ? startingDay : 0;
+				for (j; j <= 6; j++) {
+					if (day <= monthLength) {
+// 						let counter = i + 1;
+						let currentRow = document.getElementById('row' + i);
+						let currentCell = document.getElementById('cell' + i + j);
+						currentCell.innerHTML = day;
+						day++;
+					}
+					
+					if (day > monthLength) {
+						break;
+					}
+				}
+			}
+		}
+		
+		fillTheRows();
 	}
-
 }
+
+
+
+
+
+
 
 
 
